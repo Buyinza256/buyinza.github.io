059094bbe5b364748f3783602da6b06b276a6b34
Buyinza Briton Joshua <81907505+Buyinza256@users.noreply.github.com>
2023-07-07 10:56:23 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import "bulma/css/bulma.min.css";
+
+$(window).scroll(function (e) {
+  var $el = $("#.top-nav");
+  var isPositionFixed = $el.css("position") == "fixed";
+  if ($(this).scrollTop() > 400 && !isPositionFixed) {
+    $el.css({ position: "fixed", top: "0px" });
+  }
+  if ($(this).scrollTop() < 400 && isPositionFixed) {
+    $el.css({ position: "static", top: "0px" });
+  }
+});
